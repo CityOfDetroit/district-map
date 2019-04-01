86b49e409f6a9462d8a58954ff4d97d6b1766981
akhila.errabelly <aerrabelly@gmail.com>
2019-04-01 12:23:45 -0400

    Added API to the panel

--- components/panel.class.js
+++ components/panel.class.js
@@ -1,5 +1,6 @@
 'use strict';
 const moment = require('moment');
+const jQuery = require('jquery');
 export default class Panel {
     constructor(container){
         this.container = container;
@@ -15,10 +16,59 @@ export default class Panel {
 
     buildMarkUp(data){
         let html = `
-            <h2>${data.properties.Name}</h2>
-            <p><strong>Section:</strong> ${data.properties.Section}</p>
-            <p><strong>Year Enacted:</strong> ${moment(data.properties.Year_Enacted).format('MMMM Do, YYYY')}</p>
+        <h2>${data.properties.Name}</h2>
+        <p><strong>Section:</strong> ${data.properties.Section}</p>
+        <p><strong>Year Enacted:</strong> ${moment(data.properties.Year_Enacted).format('MMMM Do, YYYY')}</p>
+        <div id="members-information">
+            <div id="district-managers"></div>
+            <div id="council-members"></div>
+            <div id="district-inspectors"></div>
+        </div>
         `;
+        jQuery.ajax({
+            method: 'GET',
+            url: 'https://detroitmi.gov/rest/district-managers?_format=hal_json'
+        }).done(function (data) {
+            if(data && data.length) {
+                let managersHtml = '<h1>Managers</h1><ul>'
+                data.forEach((manager) => {
+                    managersHtml += `<li>${manager.title}</li>`;
+                });
+                managersHtml += '</ul>';
+                jQuery('#district-managers').html(managersHtml);
+            }
+        }).fail(function (error) {
+        });
+
+        jQuery.ajax({
+            method: 'GET',
+            url: 'https://detroitmi.gov/rest/council-members?_format=hal_json'
+        }).done(function (data) {
+            if(data && data.length) {
+                let councilMembersHtml = '<h1>Council Members</h1><ul>'
+                data.forEach((member) => {
+                    councilMembersHtml += `<li>${member.field_organization_head_name}</li>`;
+                });
+                councilMembersHtml += '</ul>';
+                jQuery('#council-members').html(councilMembersHtml);
+            }
+        }).fail(function (error) {
+        });
+
+        jQuery.ajax({
+            method: 'GET',
+            url: 'https://detroitmi.gov/rest/district-inspectors?_format=hal_json'
+        }).done(function (data) {
+            if(data && data.length) {
+                let inspectorsHtml = '<h1>Managers</h1><ul>'
+                data.forEach((inspector) => {
+                    inspectorsHtml += `<li>${inspector.title}</li>`;
+                });
+                inspectorsHtml += '</ul>';
+                jQuery('#district-inspectors').html(inspectorsHtml);
+            }
+        }).fail(function (error) {
+        });
         
         return html;
     }
