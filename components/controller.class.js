069cb3d651e55b730e4a4cc1e5a7a3589051ef7c
Edgar Montes <jedgar1mx@gmail.com>
2019-07-03 16:07:40 -0400

    removing active panel on parks,schools and libraries.

--- components/controller.class.js
+++ components/controller.class.js
@@ -244,36 +244,36 @@ export default class Controller {
     this.panel.buildPanel(ev);
   }
 
-  geoResults(ev, controller){
-    controller.map.geocoder.setInput('');
-    controller.map.map.getSource('single-point').setData(ev.result.geometry);
-    controller.map.map.flyTo({
-      center: ev.result.center,
-      zoom: 15,
-      speed: 1,
-      curve: 1,
-      easing(t) {
-        return t;
-      }
-    });
-    const url = `https://services2.arcgis.com/qvkbeam7Wirps6zC/ArcGIS/rest/services/Detroit_Local_Historic_Districts/FeatureServer/0/query?where=&text=&objectIds=&time=&geometry=${ev.result.center[0]}%2C+${ev.result.center[1]}&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=fid%2C+name&returnGeometry=false&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&f=json`;
-    fetch(url)
-    .then((resp) => resp.json())
-    .then(function (data) {
-      //console.log(data);
-      if (data.features.length) {
-        const patrol = data.features[0].properties.name.split(' ').join('+');
-        document.getElementById('sheet-link').href = `https://app.smartsheet.com/b/form/f004f42fcd4345b89a35049a29ff408a?Patrol+ID=${data.features[0].properties.FID}&Patrol+Name=${patrol}`;
-        document.querySelector('.patrol-info').innerHTML = `<h3>Radio Patrol ${data.features[0].properties.name}</h3><p>Interested in becoming part of your local radio patrol? Follow the link to start the process.</p><p><small>The Radio Patrol application process is managed by the Detroit Police Department. Once you complete the sign up, someone will contact you regarding the application process. Residents who complete the online form will be contacted after October 31 to start the application process.</small></p>`;
-        document.querySelector('.data-panel').className = 'data-panel active';
-        controller.geocoderOff = true;
-      } else {
-        const patrol = 'NEED+NAME';
-        document.getElementById('sheet-link').href = `https://app.smartsheet.com/b/form/0c25bae787bc40ef9707c95b2d9684e8`;
-        document.querySelector('.patrol-info').innerHTML = `<h3>NO RADIO PATROL FOUND</h3><p>Interested starting your new local radio patrlo? Follow the link to start the process.</p><p><small>The Radio Patrol application process is managed by the Detroit Police Department. Once you complete the sign up, someone will contact you regarding the application process. Residents who complete the online form will be contacted after October 31 to start the application process.</small></p>`;
-        document.querySelector('.data-panel').className = 'data-panel active';
-        controller.geocoderOff = true;
-      }
-    });
-  }
+  // geoResults(ev, controller){
+  //   controller.map.geocoder.setInput('');
+  //   controller.map.map.getSource('single-point').setData(ev.result.geometry);
+  //   controller.map.map.flyTo({
+  //     center: ev.result.center,
+  //     zoom: 15,
+  //     speed: 1,
+  //     curve: 1,
+  //     easing(t) {
+  //       return t;
+  //     }
+  //   });
+  //   const url = `https://services2.arcgis.com/qvkbeam7Wirps6zC/ArcGIS/rest/services/Detroit_Local_Historic_Districts/FeatureServer/0/query?where=&text=&objectIds=&time=&geometry=${ev.result.center[0]}%2C+${ev.result.center[1]}&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=fid%2C+name&returnGeometry=false&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&f=json`;
+  //   fetch(url)
+  //   .then((resp) => resp.json())
+  //   .then(function (data) {
+  //     //console.log(data);
+  //     if (data.features.length) {
+  //       const patrol = data.features[0].properties.name.split(' ').join('+');
+  //       document.getElementById('sheet-link').href = `https://app.smartsheet.com/b/form/f004f42fcd4345b89a35049a29ff408a?Patrol+ID=${data.features[0].properties.FID}&Patrol+Name=${patrol}`;
+  //       document.querySelector('.patrol-info').innerHTML = `<h3>Radio Patrol ${data.features[0].properties.name}</h3><p>Interested in becoming part of your local radio patrol? Follow the link to start the process.</p><p><small>The Radio Patrol application process is managed by the Detroit Police Department. Once you complete the sign up, someone will contact you regarding the application process. Residents who complete the online form will be contacted after October 31 to start the application process.</small></p>`;
+  //       document.querySelector('.data-panel').className = 'data-panel active';
+  //       controller.geocoderOff = true;
+  //     } else {
+  //       const patrol = 'NEED+NAME';
+  //       document.getElementById('sheet-link').href = `https://app.smartsheet.com/b/form/0c25bae787bc40ef9707c95b2d9684e8`;
+  //       document.querySelector('.patrol-info').innerHTML = `<h3>NO RADIO PATROL FOUND</h3><p>Interested starting your new local radio patrlo? Follow the link to start the process.</p><p><small>The Radio Patrol application process is managed by the Detroit Police Department. Once you complete the sign up, someone will contact you regarding the application process. Residents who complete the online form will be contacted after October 31 to start the application process.</small></p>`;
+  //       document.querySelector('.data-panel').className = 'data-panel active';
+  //       controller.geocoderOff = true;
+  //     }
+  //   });
+  // }
 }
